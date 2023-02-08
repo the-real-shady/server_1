0d7d440f512f4c92de2716fa3eae3dd3c935d467
the-real-shady <39212090+the-real-shady@users.noreply.github.com>
2023-02-08 10:11:27 +0300

    Update main.py

--- Client/main.py
+++ Client/main.py
@@ -1,7 +1,9 @@
 import socket
+import sys
 
+ip = sys.argv[-1]
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client_socket.connect(('10.171.132.172', 5000))
+client_socket.connect((ip, 5000))
 time = client_socket.recv(1024).decode()
 print(time)
 client_socket.close()
