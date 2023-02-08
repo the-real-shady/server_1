89fab541f4be85275b5768f89f494a4d58431d41
the-real-shady <39212090+the-real-shady@users.noreply.github.com>
2023-02-08 09:36:34 +0300

    Update and rename oleg.py to server.py

--- server/server.py
+++ server/server.py
@@ -1,8 +1,9 @@
 import socket
 from datetime import datetime
 
+ip = '10.171.132.172' # Enter your ip server ip here
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_socket.bind(('10.171.132.172', 5000))
+server_socket.bind((ip, 5000))
 server_socket.listen()
 
 while True:
